603b8187872a2a2e4a5dc6224a0d6923964d06f7
lunox94 <gmrlunox94@gmail.com>
2022-01-04 13:14:28 -0500

    chore: remove service loader feature

--- lib/src/application/core/service_loader_bloc/service_loader_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'service_loader_bloc.dart';
-
-@freezed
-class ServiceLoaderEvent with _$ServiceLoaderEvent {
-  const factory ServiceLoaderEvent.servicesLoadRequested(
-      {required List<ServiceLoad> serviceLoads}) = _ServicesLoadRequested;
-}
