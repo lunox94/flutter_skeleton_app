d41c10ddf48c89a56b0c1eccbf50013ee29d2136
lunox94 <gmrlunox94@gmail.com>
2022-01-04 16:34:00 -0500

    feat: set up local cache, implemented for theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'src/presentation/core/app.dart';
 
-void main() {
-  runApp(Bootstrapper.dev().app);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  HydratedBlocOverrides.runZoned(
+    () => runApp(Bootstrapper.dev().app),
+    storage: storage,
+  );
 }
--- lib/src/application/core/theme_bloc/theme_bloc.dart
+++ lib/src/application/core/theme_bloc/theme_bloc.dart
@@ -1,11 +1,13 @@
-import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 part 'theme_bloc.freezed.dart';
 
-class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+enum _ThemeMode { light, dark, system }
+
+class ThemeBloc extends HydratedBloc<ThemeEvent, ThemeState> {
   ThemeBloc() : super(const _SystemMode()) {
     on<ThemeEvent>((event, emit) {
       event.when(
@@ -14,5 +16,30 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
         systemModeRequested: () => emit(const ThemeState.systemMode()),
       );
     });
+  }
+
+  @override
+  ThemeState? fromJson(Map<String, dynamic> json) {
+    final idx = json['mode'] as int;
+    final mode = _ThemeMode.values[idx];
+
+    switch (mode) {
+      case _ThemeMode.light:
+        return const ThemeState.lightMode();
+      case _ThemeMode.dark:
+        return const ThemeState.darkMode();
+      case _ThemeMode.system:
+        return const ThemeState.systemMode();
+      default:
+        return null;
+    }
+  }
+
+  @override
+  Map<String, dynamic>? toJson(ThemeState state) {
+    return state.when(
+        lightMode: () => {'mode': _ThemeMode.light.index},
+        darkMode: () => {'mode': _ThemeMode.dark.index},
+        systemMode: () => {'mode': _ThemeMode.system.index});
   }
 }
