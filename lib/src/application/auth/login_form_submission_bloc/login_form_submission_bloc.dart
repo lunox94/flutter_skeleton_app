cb5e312b5468d3bab0164637cad722116337fe55
lunox94 <gmrlunox94@gmail.com>
2022-01-07 00:15:43 -0500

    feat: map corresponding states when submitting login form

--- lib/src/application/auth/login_form_submission_bloc/login_form_submission_bloc.dart
+++ lib/src/application/auth/login_form_submission_bloc/login_form_submission_bloc.dart
@@ -1,14 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_skeleton_app/src/application/auth/login_params.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../auth_facade.dart';
 
 part 'login_form_submission_event.dart';
 part 'login_form_submission_state.dart';
 part 'login_form_submission_bloc.freezed.dart';
 
-class LoginFormSubmissionBloc extends Bloc<LoginFormSubmissionEvent, LoginFormSubmissionState> {
-  LoginFormSubmissionBloc() : super(_Initial()) {
-    on<LoginFormSubmissionEvent>((event, emit) {
-      // TODO: implement event handler
+class LoginFormSubmissionBloc
+    extends Bloc<LoginFormSubmissionEvent, LoginFormSubmissionState> {
+  final AuthFacade _authFacade;
+
+  LoginFormSubmissionBloc({required AuthFacade authFacade})
+      : _authFacade = authFacade,
+        super(const _Initial()) {
+    on<LoginFormSubmissionEvent>((event, emit) async {
+      emit(const LoginFormSubmissionState.inProgress());
+      try {
+        await _authFacade.loginWithEmailAndPassword(event.loginParams);
+        emit(const LoginFormSubmissionState.success());
+      } catch (e) {
+        emit(const LoginFormSubmissionState.failure());
+      }
     });
   }
 }
--- lib/src/application/auth/login_form_submission_bloc/login_form_submission_event.dart
+++ lib/src/application/auth/login_form_submission_bloc/login_form_submission_event.dart
@@ -2,6 +2,6 @@ part of 'login_form_submission_bloc.dart';
 
 @freezed
 class LoginFormSubmissionEvent with _$LoginFormSubmissionEvent {
-  const factory LoginFormSubmissionEvent.loginFormSubmissionRequested() =
-      _LoginFormSubmissionRequested;
+  const factory LoginFormSubmissionEvent.loginFormSubmissionRequested(
+      {required LoginParams loginParams}) = _LoginFormSubmissionRequested;
 }
